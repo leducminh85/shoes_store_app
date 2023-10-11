6f759b5f344d6f975af7b10f7f5d70f28deaccc4
Đức Minh <66857266+leducminh85@users.noreply.github.com>
2023-10-11 21:18:36 +0700

    change bottom nav bar

--- lib/components/coustom_bottom_nav_bar.dart
+++ lib/components/coustom_bottom_nav_bar.dart
@@ -49,12 +49,11 @@ class CustomBottomNavBar extends StatelessWidget {
                     Navigator.pushNamed(context, HomeScreen.routeName),
               ),
               IconButton(
-                icon: SvgPicture.asset("assets/icons/Heart Icon.svg"),
+                icon: SvgPicture.asset("assets/icons/Orders.svg"),
                 onPressed: () {},
-              ),
-              IconButton(
-                icon: SvgPicture.asset("assets/icons/Chat bubble Icon.svg"),
-                onPressed: () {},
+                color: MenuState.orders == selectedMenu
+                    ? kPrimaryColor
+                    : inActiveIconColor,
               ),
               IconButton(
                 icon: SvgPicture.asset(
